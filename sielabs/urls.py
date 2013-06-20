6578be2edc203c415c3c82f50164c33a11b81802
Roger <Roger.Mendez.R@gamil.com>
2013-06-20 18:00:07 -0400

    para heroku

--- sielabs/urls.py
+++ sielabs/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import patterns, include, url
+from django.contrib import admin
+from django.conf import settings
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+admin.autodiscover()
+
 
 urlpatterns = patterns('',
     # Examples:
@@ -18,4 +21,5 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
 )
